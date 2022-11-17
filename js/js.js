d18f4662001e576e6393a4ff62c565de2a72fcaa
VitaHasten <holgerstein@hotmail.com>
2022-11-17 14:07:41 +0100

    Lagt till en massa @media
    Ser bra ut nu!

--- js/js.js
+++ js/js.js
@@ -1,15 +1,34 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
+
+function buttonOne(){
+  myFunction("one");
+}
+function buttonTwo(){
+  myFunction("two");
+}
+
+// Denna funktion är hämtad från W3Schools-mall.
+function myFunction(buttonName) {
+    if(buttonName == "one")
+      {
+        var dots = document.getElementById("dots1");
+        var moreText = document.getElementById("more1");
+        var btnText = document.getElementById("myBtn1");
+      }
+      else{
+        var dots = document.getElementById("dots2");
+        var moreText = document.getElementById("more2");
+        var btnText = document.getElementById("myBtn2");
+      }
+    
     if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Förminska";
+      btnText.innerHTML = "Läs mer";
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
-      btnText.innerHTML = "Läs mer";
+      btnText.innerHTML = "Förminska";
       moreText.style.display = "inline";
     }
   }
+
+  
