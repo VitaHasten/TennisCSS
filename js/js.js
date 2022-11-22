cfafb248d5229ac068b4a8a69aac59f7bf8177e6
VitaHasten <holgerstein@hotmail.com>
2022-11-22 12:50:38 +0100

    Fått fungerande hamburgermeny
    Fungerar helt ok i förminskad vy. Vill ändra färg på hamburgaren till vit.

--- js/js.js
+++ js/js.js
@@ -31,4 +31,12 @@ function myFunction(buttonName) {
     }
   }
 
+  const button = document.getElementById("my-button");
+const nav = document.getElementById("mynav");
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('show');
+});
+console.log(button.innerHTML);
+
   
